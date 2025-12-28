6d08aa2bed3ffc4a6e6004c46f4ff23aa78aa97a
kelaaah <martin.previte93@hotmail.com>
2025-12-27 23:05:34 -0300

    Fix updater permisos - requiere admin

---
+++ MystoviaLauncher-Updater.bat
@@ -0,0 +1,13 @@
+@echo off
+:: Mystovia Launcher with Auto-Update
+:: This script checks for updates before launching
+
+cd /d "%~dp0"
+
+:: Run updater script with admin privileges
+powershell.exe -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0update-client.ps1\"' -Verb RunAs -Wait"
+
+:: Launch the actual launcher
+start "" "%~dp0MystoviaLauncher.exe"
+
+exit
